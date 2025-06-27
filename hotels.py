d88cbec332087e2b2ecf64166fc4925b4ac8a201
e.balashov <e.balashov@maxima.ru>
2025-06-27 18:24:44 +0300

    Вынесены в отдельные файлы модели и ручки

---
+++ hotels.py
@@ -0,0 +1,65 @@
+
+
+from fastapi import APIRouter
+
+from sсhemas.hotel import Hotel, HotelPATCH
+
+router = APIRouter(prefix='/hotels',tags=["Отели"])
+
+
+hotels = [
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Дубай", "name": "dubai"},
+    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 5, "title": "Москва", "name": "moscow"},
+    {"id": 6, "title": "Казань", "name": "kazan"},
+    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
+
+]
+
+
+@router.get("/hotels")
+def get_hotels():
+    return [hotel for hotel in hotels]
+
+
+@router.post("")
+def add_hotel(hotel_data: Hotel,
+
+              ):
+    hotel_id = hotels[-1]["id"]
+    hotels.append(
+        {"id": hotel_id+1,
+         "title": hotel_data.title,
+         "name": hotel_data.name, }
+    )
+    return {"status": "success", "result": [hotel for hotel in hotels]}
+
+
+@router.put("{hotel_id}")
+def put_hotel(
+    hotel_id: int,
+    hotel_data: Hotel,
+):
+    global hotels
+    for hotel in hotels:
+        if hotel_id == hotel["id"]:
+            hotel["title"] = hotel_data.title
+            hotel["name"] = hotel_data.name
+    return {"status": "success", "result": [hotel for hotel in hotels]}
+
+
+@router.patch("{hotel_id}")
+def path_hotel(
+    hotel_id: int,
+    hotel_data: HotelPATCH,
+):
+    global hotels
+    for hotel in hotels:
+        if hotel_id == hotel["id"]:
+            if hotel_data.title:
+                hotel["title"] = hotel_data.title
+            if hotel_data.name:
+                hotel["name"] = hotel_data.name
+    return {"status": "success", "result": [hotel for hotel in hotels]}
--- main.py
+++ main.py
@@ -1,54 +1,12 @@
 
 
-from fastapi import Body, FastAPI, Query
+from fastapi import FastAPI
 import uvicorn
+from hotels import router as router_hotels
 
 
 app = FastAPI()
-
-
-hotels = [
-    {"id": 1, "title": "Sochi", "name": "Сочи"},
-    {"id": 2, "title": "Дубай", "name": "Дубай"},
-]
-
-
-@app.get("/hotels")
-def get_hotels():
-    return [hotel for hotel in hotels]
-
-
-@app.put("/hotels/{hotel_id}")
-def put_hotel(
-    hotel_id: int,
-    title: str = Body(description="Название отеля"), 
-    name: str= Body(description="Имя Отеля"),
-):
-    global hotels
-    for hotel in hotels:
-        if hotel_id == hotel["id"]:
-            hotel["title"]=title
-            hotel["name"]=name
-        else: return {"status": "false", "result":f"hotel_id:{hotel_id} not found"}
-    return {"status": "success", "result":[hotel for hotel in hotels]}
-
-@app.patch("/hotels/{hotel_id}")
-def path_hotel(
-    hotel_id: int,
-    title: str | None = Body(None, description="Название отеля"),
-    name: str | None= Body(None,description="Имя Отеля")
-
-):
-    global hotels
-    for hotel in hotels:
-        if hotel_id == hotel["id"]:
-            if title:
-                hotel["title"]=title
-            if name:
-                hotel["name"]=name
-        else: return {"status": "false", "result":f"hotel_id:{hotel_id} not found"}
-    return {"status": "success", "result":[hotel for hotel in hotels]}
-
+app.include_router(router_hotels)
 
 
 if __name__ == "__main__":
