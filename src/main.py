e115d95a2076c5a9b108cbaa1ce47d18ea31739f
e.balashov <e.balashov@maxima.ru>
2025-06-28 15:57:10 +0300

    Добавление в проект .env

--- src/main.py
+++ src/main.py
@@ -1,19 +1,18 @@
 
 
-from fastapi import FastAPI
-import uvicorn
-
-
-import sys
 from pathlib import Path
+import sys
+import uvicorn
+from fastapi import FastAPI
 
 sys.path.append(str(Path(__file__).parent.parent))
+from src.config import settings
 from src.api.hotels import router as router_hotels
+
+
 
 app = FastAPI()
 app.include_router(router_hotels)
-
-
 
 
 if __name__ == "__main__":
