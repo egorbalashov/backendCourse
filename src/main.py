d9dbfb0d3394fc9f6c743bee16b666e95a5e0147
e.balashov <e.balashov@maxima.ru>
2025-06-29 16:43:31 +0300

    Настроено подключение к БД

--- src/main.py
+++ src/main.py
@@ -6,9 +6,9 @@ import uvicorn
 from fastapi import FastAPI
 
 sys.path.append(str(Path(__file__).parent.parent))
+
 from src.config import settings
 from src.api.hotels import router as router_hotels
-
 
 
 app = FastAPI()
