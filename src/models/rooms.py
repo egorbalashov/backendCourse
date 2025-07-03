2723c2992894941d29bc414c5458a2c175a93cce
e.balashov <e.balashov@maxima.ru>
2025-07-03 11:30:55 +0300

    Настройка миграций

--- src/models/rooms.py
+++ src/models/rooms.py
@@ -9,7 +9,7 @@ class RoomsOrm(Base):
     __tablename__ = "rooms"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id"))
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id"), name="fk_rooms_hotel_id")
     title: Mapped[str]
     description: Mapped[str | None]
     price: Mapped[int]
