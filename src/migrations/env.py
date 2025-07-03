2972faae6e07910d65be839bba6f3ae136b2a1d4
e.balashov <e.balashov@maxima.ru>
2025-07-03 10:46:58 +0300

    Добавлена нова таблица rooms

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -7,6 +7,7 @@ from alembic import context
 from src.config import settings
 from src.database import Base
 from src.models.hotels import HotelsOrm
+from src.models.rooms import RoomsOrm
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
