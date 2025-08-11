02dc7f45237b92ec9bc24f84deb1599f1f818217
e.balashov <e.balashov@maxima.ru>
2025-08-11 19:45:41 +0300

    Добавлена фикстура Вынесение проверки тестовой среды в отдельную фикстуру

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,22 @@ from src.models import *
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main():
-    print("Я ФИКСТУРА")
+def check_test_mode():
+    """
+    - Проверяет тестовый режим (MODE=TEST)
+    - Иначе фиксура  async_main НЕ ЗАПУСКАЕТСЯ
+    """
     assert settings.MODE == "TEST"
 
+
+@pytest.fixture(scope="session",    # scope: определяет время жизни фикстуры:
+                                    # "function" - для каждой тест-функции (по умолчанию)
+                                    # "class" - для каждого тест-класса
+                                    # "module" - для каждого модуля с тестами
+                                    # "session" - один раз на все тесты (как в этом случае)
+                
+                autouse=True)
+async def async_main(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
